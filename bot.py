fb87a78afd7aa50d7cee106b81624010c6bc7915
miksuxo <mikaelyk@outlook.com>
2025-03-28 16:19:02 +0200

    bot update

--- bot.py
+++ bot.py
@@ -1,9 +1,17 @@
 import discord
 from discord.ext import commands
+from googleapiclient.discovery import build
 
-# Set up the bot with a command prefix
+# type your bot token below
+TOKEN = "YOUR_BOT_TOKEN"
+#type your youtube api key below
+YTAPI = "YOUR_YOUTUBE_API_KEY"
+
 intents = discord.Intents.default()
+intents.message_content = True
+
 bot = commands.Bot(command_prefix='!', intents=intents)
+youtube = build('youtube', 'v3', developerKey=YTAPI)
 
 @bot.event
 async def on_ready():
@@ -14,5 +22,19 @@ async def help(ctx):
     commands_list = "\n".join(["!help - Lists available commands"])
     await ctx.send(f"Available commands:\n{commands_list}")
 
-# Run the bot (replace 'YOUR_BOT_TOKEN' with your actual bot token)
-bot.run('YOUR_BOT_TOKEN')
+@bot.command()
+async def yt(ctx, *, searchword: str):
+    request = youtube.search().list(
+        part="snippet",
+        q=searchword,
+        type="video",
+        maxResults=1
+    )
+    response = request.execute()
+
+    video_id = response['items'][0]['id']['videoId']
+    video_url = f"https://www.youtube.com/watch?v={video_id}"
+
+    await ctx.send({video_url})
+
+bot.run(TOKEN)
